1ab6dc85214eaf0245e8de93ac36ff59ce43486c
mattellis91 <mathew.ellis2@griffithuni.edu.au>
2023-09-29 11:16:26 +1000

    added basic memory handling

--- chunk.h
+++ chunk.h
@@ -1,6 +1,6 @@
 #ifndef clox_chunk_h
-
 #define clox_chunk_h
+
 #include "common.h"
 
 typedef enum {
@@ -12,5 +12,8 @@ typedef struct {
     int capacity;
     uint8_t* code;
 } Chunk;
+
+void initChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte);
 
 #endif
